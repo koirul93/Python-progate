66053b49317f9ca3cfba85528873b6fafd85c37c
Koirul <koirul.2593@gmail.com>
2021-01-07 17:38:33 +0700

    Menambahkan integer

--- dasar-dasar.py
+++ dasar-dasar.py
@@ -1,2 +1,13 @@
 # Cetak 'Hello World'
 print("Hello World")
+
+
+# integer
+# Cetak 7 sebagai sebuah integer
+print(7)
+
+# Cetak penjumlahan dari 9 dan 3
+print(9 + 3)
+
+# Cetak '9 + 3' sebagai string
+print('9 + 3')
