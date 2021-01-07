0758aaba0d56b9e03b4a92a63b3f705cf5e77b90
Koirul <koirul.2593@gmail.com>
2021-01-07 17:39:31 +0700

    Menambahkan perhitungan

--- dasar-dasar.py
+++ dasar-dasar.py
@@ -11,3 +11,14 @@ print(9 + 3)
 
 # Cetak '9 + 3' sebagai string
 print('9 + 3')
+
+
+# Perhitungan
+# Cetak hasil dari 9 / 2
+print(9 / 2)
+
+# Cetak hasil dar 7 * 5
+print(7 * 5)
+
+# Cetak sisa dari 5 dibagi 2 menggunakan %
+print(5 % 2)
